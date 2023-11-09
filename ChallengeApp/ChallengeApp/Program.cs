505bf513934ed50b2a59a390983a8553a393f7ac
Mariusz Donocik <1987mariuszd1987@wp.pl>
2023-11-09 18:54:33 +0100

    Lekcja4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,3 +1,60 @@
-﻿Console.WriteLine("Witaj");
-Console.ReadLine();
+﻿//var myAge = 33;
+
+int myAge = 120;
+int newAge = myAge + 5;
+int myVar2 = int.MinValue;
+uint myVar3 = uint.MaxValue;
+long myVar4 = long.MaxValue;
+ulong myVar5 = ulong.MaxValue;
+
+//liczby zmiennoprzecinkowe
+
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+decimal myNumber3 = decimal.MaxValue;
+
+//zmienne tekstowe
+string name = "Jaroslaw ";
+string surname = "K ";
+string tocoposrodku = "jego wiek to: ";
+string toconakoncu = " lat";
+string result = name + surname + tocoposrodku + myAge + toconakoncu;
+char myVar6 = 'c';
+var result2 = name.ToArray();
+
+// zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+
+var number1 = 50;
+var number2 = 10;
+
+// ==
+// !=
+// <
+// >
+// <=
+// >=
+
+var namee = "Jaroslaw K";
+var age = 55;
+
+if (age > 50)
+{
+    if (name == "Jaroslaw K")
+    {
+        Console.WriteLine("Jestem Jaroslawem K i mam kota");
+    }
+    else
+    {
+        Console.WriteLine("Cos innego");
+    }
+}
+
+// && AND
+// || OR
+// ! NOT
+
+
 
