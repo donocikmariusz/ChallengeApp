197597f383b0bb916eaee0934ddb92fcc856a92f
Mariusz Donocik <1987mariuszd1987@wp.pl>
2023-11-09 19:29:03 +0100

    Zadanie domowe 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,60 +1,25 @@
-﻿//var myAge = 33;
+﻿string name = "Jaroslaw K";
+string gender = "male";
+uint age = 65;
 
-int myAge = 120;
-int newAge = myAge + 5;
-int myVar2 = int.MinValue;
-uint myVar3 = uint.MaxValue;
-long myVar4 = long.MaxValue;
-ulong myVar5 = ulong.MaxValue;
-
-//liczby zmiennoprzecinkowe
-
-float myNumber = float.MaxValue;
-double myNumber2 = double.MaxValue;
-decimal myNumber3 = decimal.MaxValue;
-
-//zmienne tekstowe
-string name = "Jaroslaw ";
-string surname = "K ";
-string tocoposrodku = "jego wiek to: ";
-string toconakoncu = " lat";
-string result = name + surname + tocoposrodku + myAge + toconakoncu;
-char myVar6 = 'c';
-var result2 = name.ToArray();
-
-// zmienna logiczna
-bool isActive = true;
-isActive = false;
-var isValid = 5 > 6;
-
-var number1 = 50;
-var number2 = 10;
-
-// ==
-// !=
-// <
-// >
-// <=
-// >=
-
-var namee = "Jaroslaw K";
-var age = 55;
-
-if (age > 50)
+if (age == 33 && name == "Ewa" && gender == "female")
 {
-    if (name == "Jaroslaw K")
-    {
-        Console.WriteLine("Jestem Jaroslawem K i mam kota");
-    }
-    else
-    {
-        Console.WriteLine("Cos innego");
-    }
+    Console.WriteLine("Ewa, lat 33");
+}
+        else if (age < 18 && gender == "male") 
+        {
+          Console.WriteLine("Niepelnoletni Mezczyzna");
+        }
+        else if (age < 30 && gender == "female")
+        { 
+          Console.WriteLine("Kobieta ponizej 30 lat");
+        }
+else
+{
+    Console.WriteLine("Danych z wejscia nie pozna podpiac pod pkt 1,2 lub 3.");
 }
 
-// && AND
-// || OR
-// ! NOT
+
 
 
 
