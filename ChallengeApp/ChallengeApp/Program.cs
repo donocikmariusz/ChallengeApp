7ba31cc8eef605b10a3fe1f17cf8f1d4214bc0bf
Mariusz Donocik <1987mariuszd1987@wp.pl>
2023-11-10 15:33:14 +0100

    Tablice

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,14 @@
-﻿string name = "Jaroslaw K";
-string gender = "male";
-uint age = 65;
+﻿int[] grades = new int[365];
+string[] dayofWeeks = new string[7];
+dayofWeeks[0] = "poniedziałek";
+dayofWeeks[1] = "wtorek";
+dayofWeeks[2] = "środa";
+dayofWeeks[3] = "czwartek";
+dayofWeeks[4] = "piątek";
+dayofWeeks[5] = "sobota";
+dayofWeeks[6] = "niedziela";
+Console.WriteLine(dayofWeeks[2]);
 
-if (age == 33 && name == "Ewa" && gender == "female")
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-        else if (age < 18 && gender == "male") 
-        {
-          Console.WriteLine("Niepelnoletni Mezczyzna");
-        }
-        else if (age < 30 && gender == "female")
-        { 
-          Console.WriteLine("Kobieta ponizej 30 lat");
-        }
-else
-{
-    Console.WriteLine("Danych z wejscia nie pozna podpiac pod pkt 1,2 lub 3.");
-}
-
-
-
-
-
+string[] dayofWeeks2 = { "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela" };
+Console.WriteLine(dayofWeeks2[0]);
+Console.WriteLine(dayofWeeks2[6]);
