3beeb02296362ea8d93e22e5b93c246507182647
Mariusz Donocik <1987mariuszd1987@wp.pl>
2023-11-11 17:18:41 +0100

    Napisz program w C# do zamiany stopni Celcjusza na Farenheita i odwrotnie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,11 +1,39 @@
 ﻿
-Console.WriteLine("Podaj number: ");
+Console.WriteLine("1. C TO F ");
+Console.WriteLine("2. F TO C ");
+Console.WriteLine("Twój wybór :");
+
 string number = Console.ReadLine();
 Console.WriteLine(" ");
 int.TryParse(number, out int numberint);
 
-
-for (int i = numberint; i >= 0; i--)
+if (numberint == 1)
 {
-    Console.WriteLine(i);
+    Console.WriteLine("Podaj wartość w stopniach C: ");
+    string wartoscC = Console.ReadLine();
+    Console.WriteLine(" ");
+    float.TryParse(wartoscC, out float wartoscCfloat);
+
+   
+    double wynikCtoF = (wartoscCfloat * (9.0/5.0) + 32);
+    Console.WriteLine(wartoscCfloat + " stopni C to " + wynikCtoF + " stopni F ");
 }
+
+else if (numberint == 2)
+{
+    Console.WriteLine("Podaj wartość w stopniach F: ");
+    string wartoscF = Console.ReadLine();
+    Console.WriteLine(" ");
+    float.TryParse(wartoscF, out float wartoscFfloat);
+
+    
+    double wynikFtoC = (5.0/9.0) * (wartoscFfloat - 32);
+    Console.WriteLine(wartoscFfloat + " stopni F to " + wynikFtoC + " stopni C ");
+}
+
+else
+{
+    Console.WriteLine("Niewlasciwy Wybor. Try again");
+}
+
+
