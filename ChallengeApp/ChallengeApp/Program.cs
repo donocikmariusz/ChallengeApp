c8b37b36c58548603efa0786ec094e7c99877f60
Mariusz Donocik <1987mariuszd1987@wp.pl>
2023-11-13 22:41:57 +0100

    Losowanie liczb i ich zgadywanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,43 +1,32 @@
-﻿using ChallengeApp;
+﻿using System;
 
-Employee user1 = new Employee("Zbigniew", "Stonoga", "54");
-Employee user2 = new Employee("Jarosław", "Kaczynski", "65");
-Employee user3 = new Employee("Mateusz", "Morawiacki", "49");
+Random generator = new Random();
 
-user1.AddScore(5);
-user1.AddScore(5);
-user1.AddScore(4);
-user1.AddScore(3);
-user1.AddScore(5);
+int numberRandom = generator.Next(1, 100);
+int ileprób = 0;
 
-user2.AddScore(1);
-user2.AddScore(2);
-user2.AddScore(3);
-user2.AddScore(4);
-user2.AddScore(5);
-
-user3.AddScore(5);
-user3.AddScore(5);
-user3.AddScore(3);
-user3.AddScore(4);
-user3.AddScore(1);
-
-List<Employee> list = new List<Employee>()
+while (true)
 {
-    user1, user2, user3
-};
+    Console.WriteLine("Zgadnij Liczbe: ");
+    int userNumber = int.Parse(Console.ReadLine());
 
-int maxResult = -1;
-Employee userwithMaxValue = null;
-
-foreach (var user in list)
-{
-    if (user.Result > maxResult)
+    if (numberRandom > userNumber)
     {
-        userwithMaxValue = user;
-        maxResult = user.Result;
+        Console.WriteLine("Za mała. Try Again");
+        ileprób++;
+    }
+
+    else if (numberRandom < userNumber)
+    {
+        Console.WriteLine("Za duża. Try Again");
+        ileprób++;
+    }
+    else
+    {
+        Console.WriteLine("Dobrze, liczba prób: " + ileprób);
+        break;
     }
 }
 
-Console.WriteLine("Najwyzsza ilośc punktów ma : " + userwithMaxValue.Name +
-    " " + userwithMaxValue.Surname + " ilość punktów: " + userwithMaxValue.Result);
+
+
