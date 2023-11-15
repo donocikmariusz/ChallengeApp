d08b7136cf96124c6036f1d57672d273e4fd1834
Mariusz Donocik <1987mariuszd1987@wp.pl>
2023-11-15 18:33:51 +0100

    Zad 9

---
+++ ChallengeApp/ChallengeApp.tests/EmloyeeTests.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ChallengeApp.tests
+{
+    public class EmloyeeTests
+    {
+        [Test]
+        public void MinTest()
+        {
+            var employee = new Employee("Parch", "Naczelny");
+            employee.AddGrades(2);
+            employee.AddGrades(2);
+            employee.AddGrades(6);
+
+            var statistics = employee.GetStatistics();
+            Assert.AreEqual(2, statistics.Min);
+
+        }
+
+        [Test]
+        public void MaxTest()
+        {
+            var employee1 = new Employee("Jarosław", "Kaczyński");
+            employee1.AddGrades(4);
+            employee1.AddGrades(2);
+            employee1.AddGrades(6);
+
+            var statistics = employee1.GetStatistics();
+            Assert.AreEqual(6, statistics.Max);
+        }
+
+        [Test]
+        public void AverageTest()
+        {
+            var employee2 = new Employee("Zbigniew", "Stonoga");
+            employee2.AddGrades(4);
+            employee2.AddGrades(2);
+            employee2.AddGrades(6);
+
+            var statistics = employee2.GetStatistics();
+            Assert.AreEqual(4.0, statistics.Average);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,43 +1,13 @@
 ﻿using ChallengeApp;
 
-Employee user1 = new Employee("Zbigniew", "Stonoga", "54");
-Employee user2 = new Employee("Jarosław", "Kaczynski", "65");
-Employee user3 = new Employee("Mateusz", "Morawiacki", "49");
+var employee = new Employee("Parch", "Naczelny");
+employee.AddGrades(2);
+employee.AddGrades(2);
+employee.AddGrades(6);
 
-user1.AddScore(5);
-user1.AddScore(5);
-user1.AddScore(4);
-user1.AddScore(3);
-user1.AddScore(5);
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Average : {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
 
-user2.AddScore(1);
-user2.AddScore(2);
-user2.AddScore(3);
-user2.AddScore(4);
-user2.AddScore(5);
 
-user3.AddScore(5);
-user3.AddScore(5);
-user3.AddScore(3);
-user3.AddScore(4);
-user3.AddScore(1);
-
-List<Employee> list = new List<Employee>()
-{
-    user1, user2, user3
-};
-
-int maxResult = -1;
-Employee userwithMaxValue = null;
-
-foreach (var user in list)
-{
-    if (user.Result > maxResult)
-    {
-        userwithMaxValue = user;
-        maxResult = user.Result;
-    }
-}
-
-Console.WriteLine("Najwyzsza ilośc punktów ma : " + userwithMaxValue.Name +
-    " " + userwithMaxValue.Surname + " ilość punktów: " + userwithMaxValue.Result);
